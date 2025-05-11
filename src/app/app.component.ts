f2d5eb705c54216f70e547318ca4f0dd026aa25a
Yuri Leal da Cruz <100494057+yurilealdacruz@users.noreply.github.com>
2025-05-10 21:40:01 -0300

    Lista Comum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaComponent } from "./lista/lista.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
