1e9c70c611989b3b68f5b387355669f937347f2e
Yuri Leal da Cruz <100494057+yurilealdacruz@users.noreply.github.com>
2025-05-10 22:16:29 -0300

    Adicionando projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListaComponent } from "./lista/lista.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [ListaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'lis-basica';
+}
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lista',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './lista.component.html',
+  styleUrl: './lista.component.scss'
+})
+export class ListaComponent {
+ item: string = '';
+  lista: ItemLista[] = [];
+
+
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    
+    if(this.item){
+      itemLista.nome = this.item;
+      itemLista.id = this.lista.length + 1;
+      this.lista.push(itemLista)
+    }
+    this.item = '';
+  }
+
+  ricarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+    
+  }
+
+  limpar() {
+    this.lista = [];
+  }
+}
